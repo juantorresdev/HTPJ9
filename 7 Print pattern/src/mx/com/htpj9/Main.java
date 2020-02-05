abdebf24a9d0ef1fd20e84c97b400d546f30dbe8
juantorresdev <57916724+juantorresdev@users.noreply.github.com>
2020-02-04 22:05:01 -0600

    Java 6th and 7th programs
    Java Exercises from section 2.

---
+++ 7 Print pattern/src/mx/com/htpj9/Main.java
@@ -0,0 +1,17 @@
+package mx.com.htpj9;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int limit = 8;
+
+        for(int i = 0; i < limit; i++){
+            if(i % 2 != 0){
+                System.out.print(" ");
+            }
+            System.out.println("* * * * * * * *");
+        }
+
+    }
+}
---
+++ 8 Table of squares and cubes/src/mx/com/htpj9/Main.java
@@ -0,0 +1,14 @@
+package mx.com.htpj9;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.printf("number\tsquare\tcube\n");
+
+        for(int i = 0; i <= 10; i++){
+            System.out.printf("%d\t\t%d\t\t%d\n", i, i*i, i*i*i);
+        }
+
+    }
+}
